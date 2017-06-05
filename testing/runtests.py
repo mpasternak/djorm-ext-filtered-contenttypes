540925c916f7c534a4841a61e6a03ba02a829060
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-06-05 12:51:15 +0200

    Quiet

--- testing/runtests.py
+++ testing/runtests.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     django.setup()
 
     args = sys.argv[1:]
-    call_command("test", 'filtered_contenttypes', *args, verbosity=2, interactive=False)
+    call_command("test", 'filtered_contenttypes', *args, verbosity=0, interactive=False)
