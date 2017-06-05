606a2d7e22bbdf282d23da47db21c5b96a05a669
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-06-05 13:00:45 +0200

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ GenericForeignKey with filtering capabilities for Django ORM + PostgreSQL
 
 setup(
     name="djorm-ext-filtered-contenttypes",
-    version='0.3.0',
+    version='0.3.1',
     url='https://github.com/mpasternak/djorm-ext-filtered-contenttypes',
     license='MIT',
     platforms=['OS Independent'],
