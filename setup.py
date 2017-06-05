8c1f0e8970fd3ece688074f9bbfa9808a2545448
Michał Pasternak <mpasternak@MacBook-Pro-Michala.local>
2017-06-05 13:00:13 +0200

    Use bump version

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 from setuptools import setup, find_packages
 
-description="""
+description = """
 GenericForeignKey with filtering capabilities for Django ORM + PostgreSQL
 """
 
 setup(
-    name = "djorm-ext-filtered-contenttypes",
-    version = '0.3',
-    url = 'https://github.com/mpasternak/djorm-ext-filtered-contenttypes',
-    license = 'MIT',
-    platforms = ['OS Independent'],
-    description = description.strip(),
-    author = 'Michal Pasternak',
-    author_email = 'michal.dtz@gmail.com',
-    maintainer = 'Michal Pasternak',
-    maintainer_email = 'michal.dtz@gmail.com',
-    packages = ["filtered_contenttypes"],
-    include_package_data = False,
-    install_requires = [],
-    zip_safe = False,
-    classifiers = [
+    name="djorm-ext-filtered-contenttypes",
+    version='0.3',
+    url='https://github.com/mpasternak/djorm-ext-filtered-contenttypes',
+    license='MIT',
+    platforms=['OS Independent'],
+    description=description.strip(),
+    author='Michal Pasternak',
+    author_email='michal.dtz@gmail.com',
+    maintainer='Michal Pasternak',
+    maintainer_email='michal.dtz@gmail.com',
+    packages=["filtered_contenttypes"],
+    include_package_data=False,
+    install_requires=[
+        "Django>=1.7,<=1.10"
+    ],
+    zip_safe=False,
+    classifiers=[
         "Operating System :: OS Independent",
         "Environment :: Web Environment",
         "Framework :: Django",
@@ -29,7 +31,7 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries",
         "Topic :: Utilities",
     ]
